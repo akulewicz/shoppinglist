6ffda6a75ab40799949119a610cacf32c8f44175
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2020-06-14 13:21:04 +0200

    Add feature: displaying shopping list

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const form = document.querySelector('.form--js');
-
+const productsList = document.querySelector('.products-list--js');
 const products = [];
 
+const displayShoppingList = () => {
+    let html = '';
+    products.forEach(product => {
+        html += `<li class="products-list__item">${product}<i class="far fa-trash-alt delete"></i></li>`;
+    })
+    productsList.innerHTML = html;
+}
+
 const addProduct = (newProduct) => {
-    if(newProduct.length) {
+    if (newProduct.length) {
         products.push(newProduct);
     }
     form.reset();
-    console.log(products);
+    displayShoppingList();
 }
 
 form.addEventListener('submit', e => {

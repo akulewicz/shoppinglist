742c57127f0d87c3a78d77b9935ab6093c1a7eeb
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2020-06-18 22:29:50 +0200

    Feat: mark the purchased product

--- modules/functions.js
+++ modules/functions.js
@@ -1,15 +1,29 @@
-import {productsBox, productsList, form} from '../main.js';
-export {displayShoppingList, addProduct, deleteProduct};
+import {
+    productsBox,
+    productsList,
+    form
+} from '../main.js';
+export {
+    displayShoppingList,
+    addProduct,
+    deleteProduct
+};
 
 const displayShoppingList = () => {
     productsBox.textContent = '';
-    productsList.forEach(product => {
+    productsList.forEach((product, index) => {
         const li = document.createElement('li');
         const i = document.createElement('i');
+        const span = document.createElement('span');
         li.classList.add('products-list__item');
-        i.classList.add('far', 'fa-trash-alt', 'products-list__delete-icon')
-        li.textContent = product;
+        li.dataset.productId = index;
+        span.textContent = product.name;
+        if (product.done) {
+            span.style.textDecoration = 'line-through';
+        }
+        li.append(span);
         li.append(i);
+        i.classList.add('far', 'fa-trash-alt', 'products-list__delete-icon')
         productsBox.prepend(li);
     })
 }
@@ -23,7 +37,10 @@ const addProduct = e => {
     e.preventDefault();
     const newProduct = product.value.trim();
     if (newProduct.length) {
-        productsList.push(newProduct);
+        productsList.push({
+            name: newProduct,
+            done: false
+        });
     }
     form.reset();
     form.product.focus();
@@ -31,9 +48,15 @@ const addProduct = e => {
 }
 
 const deleteProduct = e => {
-    const product = e.target;;
+    const product = e.target;
     if (product.classList.contains('products-list__delete-icon')) {
-        productsList.splice(productsList.indexOf(product.parentNode.textContent), 1);
-        updateShoppingList();
+        productsList.splice(Math.round(product.parentNode.dataset.productId), 1);
+    } else {
+        if (productsList[Math.round(product.dataset.productId)].done) {
+            productsList[Math.round(product.dataset.productId)].done = false;
+        } else {
+            productsList[Math.round(product.dataset.productId)].done = true;
+        }
     }
+    updateShoppingList();
 }
